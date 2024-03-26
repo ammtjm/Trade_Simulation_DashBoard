c11ac2f11a34483e3d6102eb1292f908586a0129
Akira <firesisters7@gmail.com>
2024-03-26 16:18:07 +0900

    README_日本語版改修中

--- application/Input/sample_dataframe.py
+++ application/Input/sample_dataframe.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import numpy as np
 
+"""
+コメントアウトされているコードは、データフレームを作成する関数です。
+使用の際は、コメントアウトを解除してください。
 
+この関数を使用して、データフレームを作成し、ダッシュボードの入力データとして使用できます。
+ただし、ポジションフラグ、決済フラグ、数量については、ダッシュボードの入力データとして使用するために、
+適切な形式に沿ってデータフレームを作成する必要があります。
+"""
+
+
+"""
 def get_dataframe():
     data = {
         'datetime': pd.date_range(start='2023-01-01', periods=100, freq='D'),
@@ -11,3 +21,4 @@ def get_dataframe():
         'quantity': np.random.randint(1, 10, size=100)
     }
     return pd.DataFrame(data)
+"""
